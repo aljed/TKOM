990f6bad0468e1cd9d45288a9da6fbbf40502f75
aljed <a.iskandar.j@gmail.com>
2020-04-16 11:00:08 +0200

    Added Lexer files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TKOM.LexerN;
+
+namespace TKOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lexer lexer = new Lexer();
+            var list = lexer.Lex(Console.ReadLine());
+            foreach(var token in list){
+                Console.WriteLine(token.Info());
+            }
+        }
+    }
+}
