73548ec8b5d6ab5fbb58774b29234656c3a3607e
aljed <a.iskandar.j@gmail.com>
2020-04-16 17:34:05 +0200

    Chnaged Program.cs

---
+++ TKOM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using TKOM.LexerN;
+
+namespace TKOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = null;
+            if(args.Length == 1)
+            {
+                code = new StreamReader(args[0]).ReadToEnd();
+                Lex(code);
+            }
+            else{
+                Console.Write("> ");
+                while((code = Console.ReadLine()) != "exit")
+                {
+                    Lex(code);
+                    Console.Write("> ");
+                }
+            }
+        }
+
+        static void Lex(string code)
+        {
+            Lexer lexer = new Lexer();
+            var list = lexer.Lex(code);
+            int i = 0;
+            Console.WriteLine(lexer.Status.message);
+            foreach(var element in list){
+                Console.WriteLine($"ID: {(++i)}\t{element.token.Info()}\tPosition: [{element.line},{element.position}]");
+            }
+        }
+    }
+}
